133ba72e7c59bf2f7919d589ef90ee9538e25ed0
AKINBAMI OLAOLUWA ISRAEL <oakinbamiisreal@gmail.com>
2025-06-18 12:51:32 +0100

    Update egg_production_system.py

--- egg_production_system.py
+++ egg_production_system.py
@@ -90,7 +90,7 @@ st.plotly_chart(Barns, use_container_width = True)
 #BIVARIATE ANALYSIS
 st.markdown("## BIVARIATE ANALYSIS")
 st.markdown("### Year vs Organic eggs")
-df2 = pd.DataFrame(df["Number of eggs from hens in organic, free-range farms"],df["Year"])
+df2 = pd.DataFrame(df["Number of eggs from hens in organic, free-range farms"],df["Number of eggs from hens in barns"])
 st.write(df2)
 
 
